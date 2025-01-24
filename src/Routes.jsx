b53ef131794dbcaf0eaf25d0274d97dbf45de9cb
Your Name <you@example.com>
2025-01-24 17:55:14 +0330

    first commit

---
+++ src/Routes.jsx
@@ -0,0 +1,26 @@
+// import AboutMe from "./components/about/AboutMe";
+// import ContactMe from "./components/contactMe/contactMe";
+
+import Home from "./components/Home/Home";
+import Layout from "./components/Layout/Layout";
+import { createBrowserRouter } from "react-router-dom";
+
+// import Layout from "./components/Layout/Layout";
+// import NotFound from "./components/NotFound";
+// import Weblogs from "./components/weblogs/Weblogs";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      //   { path: "/aboutMe", element: <AboutMe /> },
+      //   { path: "/ContactMe", element: <ContactMe /> },
+      //   { path: "/weblogs", element: <Weblogs /> },
+      //   { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
+
+export default router;
