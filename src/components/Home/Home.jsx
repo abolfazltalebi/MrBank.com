d6785d2727d3a550dfbcd066d3e79260f25f5444
Your Name <you@example.com>
2025-01-25 16:52:37 +0330

    changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,13 @@
+import heroSection from '../../assets/images/heroSection.webp'
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <section className="container grid grid-cols-1 sm:grid-cols-2 gap-4 items-center">
+        <div>
+          <img src={heroSection} className='h-[450px]' alt="" />
+        </div>
+      </section>
+    </>
+  );
 }
